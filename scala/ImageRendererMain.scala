c30a08c730f2ac572d1cbadabc24e93d964b8139
M Saad Bin Amin <mamin.bee16seecs@seecs.edu.pk>
2020-02-02 01:24:14 +0500

    fixed a bug

--- scala/ImageRendererMain.scala
+++ scala/ImageRendererMain.scala
@@ -2,14 +2,13 @@ package software
 
 object ImageRendererMain {
   def main(args: Array[String]): Unit = {
-    utils.Files.createDirectory("swRenders")
     (4 to 12).map(1 << _).foreach(w => {
       print("Now rendering: w = " + w.toString)
       val r = new ImageRenderer(1920, 1080, w)
       val t = System.nanoTime()
       r.render()
       print ("\t\t " + ((System.nanoTime() - t) / 1e6d).toString + " (ms)\n")
-      r.save("swRenders/render" + w.toString)
+      r.save("render" + w.toString)
     })
   }
 }
